49d8999871bcace5e90d1deb5a0ee37e44bb26f2
Elaminebendaas <33202205+Elaminebendaas@users.noreply.github.com>
2023-04-26 23:31:18 -0400

    Just a message

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ if(process.env.NODE_ENV !== "production"){
 const express = require('express');
 const app = express();
 const bcrypt = require('bcryptjs');
-const LocalStrategy = require('passport-local')
+const LocalStrategy = require('passport-local').Strategy
 const mongoose = require('mongoose');
 const passport = require('passport');
 const session = require('express-session');
@@ -16,11 +16,12 @@ const cors = require('cors');
 //Models
 const User = require('./models/Users.js');
 
+
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(session({secret: process.env.SECRET, resave: false, saveUninitialized: true}));
-app.use(passport.initialize());
+
 app.use(passport.session());
 
 main().catch((err) => console.log(err));
@@ -32,20 +33,53 @@ async function main(){
 }
 
 passport.use(
-    new LocalStrategy(async(email, password, done) =>{
-        try{
-            const user = await User.findOne({email: email});
+    new LocalStrategy({usernameField: 'email',
+    passwordField: 'password'},async(username, password, done) => {
+        try {
+        console.log('used')
+        const user = await User.findOne({email: username})
+
+            console.log(user)
             if(!user){
-                return done(null, false, {message: "Incorrect username"})
-            };
+                return done(null, false, {
+                    message: "User not Found"
+                })
+            }
             bcrypt.compare(password, user.password, (err, res) => {
                 if(res){
                     return done(null, user)
+                } else{
+                    return done(err, false, {
+                        message: "Incorrect Password",
+                    })
                 }
             })
-        }
-    })
-)
+           
+        } catch(err){
+            return done(err)
+
+        }}
+        )
+
+    )
+
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+passport.deserializeUser(async function(id, done) {
+    try {
+        const user = await User.findById(id);
+        done(null, user);
+    } catch(err) {
+        done(err);
+    };
+});
+
+app.get('/', (req, res) => {
+    res.json({message: "You have landed at '/'"})
+})
 
 app.post('/signup', (req, res) => {
     bcrypt.hash(req.body.password, 10, async(err, hashedPassword) =>{
@@ -57,11 +91,14 @@ app.post('/signup', (req, res) => {
             await newUser.save();
             res.json({message: "success"});
         }
-
     }); 
 })
-
-app.post('/login', async (req,res))
+app.use(passport.initialize());
+app.post(  "/login",
+passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/failed"
+}))
 
 app.listen(5000, () => {
     console.log("The server is running on port 5000");
